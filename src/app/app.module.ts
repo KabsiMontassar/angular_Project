6a038372eab2678fb67d298111b6e91cd327cbc1
KabsiMontassar <kebsimontassar@gmail.com>
2025-02-11 11:52:33 +0100

    Revert "push"
    This reverts commit 7fbe48dbea5eb830164cc364a3a2e19c8aa6115d.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,31 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { ResidencesComponent } from './components/residences/residences.component';
-import { ResidenceDetailsComponent } from './components/residences/residence-details/residence-details.component';
-import { AddResidenceComponent } from './components/residences/add-residence/add-residence.component';
-import { ApartmentsComponent } from './components/apartments/apartments.component';
-import { ApartmentsByResidenceComponent } from './components/apartments/apartments-by-residence/apartments-by-residence.component';
-import { AddApartmentComponent } from './components/apartments/add-apartment/add-apartment.component';
-import { FormsModule } from '@angular/forms';
+import {FormsModule} from "@angular/forms";
+import { ResidencesComponentComponent } from './residences-component/residences-component.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    NotFoundComponent,
-    ResidencesComponent,
-    ResidenceDetailsComponent,
-    AddResidenceComponent,
-    ApartmentsComponent,
-    ApartmentsByResidenceComponent,
-    AddApartmentComponent
+    ResidencesComponentComponent,
+
   ],
   imports: [
     BrowserModule,
